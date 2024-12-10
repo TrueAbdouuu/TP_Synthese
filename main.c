b45e0db83cd48c249908a9b913ec60a8b3ed1783
TrueAbdouuu <aelfelsoufi@gmail.com>
2024-12-10 09:18:02 +0100

    Update main.c to handle exit code and signal display in prompt

--- main.c
+++ main.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
 #include <string.h>
-#include "enseash.h"  // Inclure l'en-tête pour utiliser les fonctions de enseash.c
+#include "enseash.h"
 
 int main() {
-    char input[INPUT_BUFFER_SIZE]; // Buffer pour l'entrée utilisateur
+    char input[INPUT_BUFFER_SIZE];
     ssize_t bytesRead;
 
-    // Affiche le message de bienvenue
+
     display_welcome_message();
+    display_prompt();
 
     while (1) {
-        // Affiche le prompt et lit l'entrée utilisateur
-        display_prompt();
+
         bytesRead = read_input(input);
 
-        // Si l'utilisateur tape "exit" ou appuie sur Ctrl+D (EOF), on quitte
-        if (bytesRead == 0 || (bytesRead > 0 && strncmp(input, "exit", 4) == 0)) {
-            write(STDOUT_FILENO, GOODBYE_MESSAGE, strlen(GOODBYE_MESSAGE));
-            break;  // Quitte la boucle et termine le programme
+
+        if (bytesRead == 0 || (bytesRead > 0 && strcmp(input, "exit") == 0)) {
+            write(STDOUT_FILENO, EXIT_MESSAGE, strlen(EXIT_MESSAGE));
+            break;
         }
 
-        // Exécute la commande entrée par l'utilisateur
+
         execute_command(input);
+        display_prompt();
     }
 
     return 0;
